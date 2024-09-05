b8d433834ee357656626f0fe6ab907a1befc13b8
sachinpandeygurh <sachingurh@gmail.com>
2024-09-05 21:54:19 +0530

    thirdcommit

--- App.js
+++ App.js
@@ -5,27 +5,27 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Welcome from './src/screen/Welcome';
 import Home from './src/screen/Home';
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { responsiveHeight, responsiveWidth } from 'react-native-responsive-dimensions';
 
 const Tab = createBottomTabNavigator();
 
 
 const SearchScreen = () => (
-  <View>
-    <Text>Search</Text>
-  </View>
+  <SafeAreaView>
+    <Text style={{ marginTop: "50%", marginLeft: "40%" }}>Search</Text>
+  </SafeAreaView>
 );
 
 const VideoScreen = () => (
-  <View>
-    <Text>Video</Text>
-  </View>
+  <SafeAreaView>
+    <Text style={{ marginTop: "50%", marginLeft: "40%" }}>Video</Text>
+  </SafeAreaView>
 );
 
 const UserScreen = () => (
   <View>
-    <Text>User</Text>
+    <Text style={{ marginTop: "50%", marginLeft: "40%" }}>User</Text>
   </View>
 );
 
@@ -38,6 +38,8 @@ export default function App() {
             let iconName;
             switch (route.name) {
               case 'Welcome':
+                iconName = 'home-sharp';
+                break;
               case 'Home':
                 iconName = 'home-sharp';
                 break;
@@ -53,15 +55,15 @@ export default function App() {
             }
 
             return (
-             <View style={{ marginLeft:responsiveWidth(70), width:responsiveWidth(90)}}>
-               <View style={[
-                { padding: 8, flexDirection: 'row',  paddingHorizontal:responsiveWidth(3) , width:responsiveWidth(15) , alignItems:"center" , justifyContent:"space-between" },
-                focused && { backgroundColor: '#FFE7EA', borderRadius: 100, width:responsiveWidth(25) }
-              ]}>
-                <Ionicons name={iconName} size={size} color={color} />
-                {focused && <Text style={{fontWeight:"600"}}>{route.name}</Text>}
+              <View style={{ marginLeft: responsiveWidth(70), width: responsiveWidth(90) }}>
+                <View style={[
+                  { padding: 8, flexDirection: 'row', paddingHorizontal: responsiveWidth(3), width: responsiveWidth(15), alignItems: "center", justifyContent: "space-between" },
+                  focused && { backgroundColor: '#FFE7EA', borderRadius: 100, width: responsiveWidth(25) }
+                ]}>
+                  <Ionicons name={iconName} size={size} color={color} />
+                  {focused && <Text style={{ fontWeight: "600" }}>{route.name}</Text>}
+                </View>
               </View>
-             </View>
             );
           },
           tabBarActiveTintColor: '#905CFF',
@@ -73,20 +75,20 @@ export default function App() {
             shadowOffset: { width: 0, height: -10 },
             shadowOpacity: 0.15,
             shadowRadius: 20,
-            elevation: 10, 
-            height:responsiveHeight(8),
-            top:responsiveHeight(1.5),
-             alignItems:"center" , justifyContent:"space-between"
+            elevation: 10,
+            height: responsiveHeight(8),
+            top: responsiveHeight(1.5),
+            alignItems: "center", justifyContent: "space-between"
           },
         })}
       >
-        
-        <Tab.Screen 
-          name="Welcome" 
-          component={Home} 
-          options={{ tabBarButton: () => null }} 
+
+        <Tab.Screen
+          name="Welcome"
+          component={Welcome}
+          options={{ tabBarButton: () => null }}
         />
-         <Tab.Screen name="Home" component={Welcome} />
+        <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Search" component={SearchScreen} />
         <Tab.Screen name="Video" component={VideoScreen} />
         <Tab.Screen name="User" component={UserScreen} />
--- src/screen/Welcome.js
+++ src/screen/Welcome.js
@@ -10,7 +10,7 @@ import { useNavigation } from '@react-navigation/native';
 const Welcome = () => {
   const navigation = useNavigation();
 const handlenavigator =()=>{
-  navigation.navigate('Welcome'); 
+  navigation.navigate('Home'); 
 }
   
 
